fe05faccf7749a3bb1ef4753d9401ba8aeeee3b5
karinacrmcldev <perfechate@gmail.com>
2022-02-04 18:36:00 +0200

    Added scss variables

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,18 @@ import React from 'react';
 import { Button } from './components/UI/Button/Button';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Button
+        type="primary"
+        onCLick={() => {
+          console.log('sdfij');
+        }}
+      >
+        ok
+      </Button>
+    </div>
+  );
 }
 
 export default App;
