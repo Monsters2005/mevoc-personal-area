b452eef8aa33dbfc0dc049ea14f11fbddcacb610
karinacrmcldev <perfechate@gmail.com>
2022-02-07 10:13:54 +0200

    PR-9: Variables corrections

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,7 @@ import React from 'react';
 import { Button } from './components/UI/Button/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <Button
-        type="primary"
-        onCLick={() => {
-          console.log('sdfij');
-        }}
-      >
-        ok
-      </Button>
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
