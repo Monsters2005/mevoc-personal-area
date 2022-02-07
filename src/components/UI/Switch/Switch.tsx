549b754eeb32b12b341c9ae8b2889424d6f2b2e4
karinacrmcldev <perfechate@gmail.com>
2022-02-07 18:50:20 +0200

    Created switch component

--- src/components/UI/Switch/Switch.tsx
+++ src/components/UI/Switch/Switch.tsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { ChangeEventHandler } from 'react';
+import s from './Switch.module.scss';
 
-export function Switch() {
-  return <div />;
+type Props = {
+  onChange: ChangeEventHandler;
+  defaultState: boolean;
+};
+
+export function Switch({ onChange, defaultState }: Props) {
+  return (
+    <div className={s.switch_container}>
+      <input
+        className={s.switch_checkbox}
+        id="switch"
+        type="checkbox"
+        checked={defaultState}
+        onChange={onChange}
+      />
+      <label className={s.switch_label} htmlFor="switch">
+        <span className={s.switch_button} />
+      </label>
+    </div>
+  );
 }
