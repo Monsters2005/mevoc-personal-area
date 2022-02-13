71d1527a0659c55dd0d7d9e326e70385892a05a7
karinacrmcldev <perfechate@gmail.com>
2022-02-13 19:06:17 +0200

    Created tabs component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import { Button } from './components/UI/Button/Button';
+import { Tabs } from './components/UI/Tabs/Tabs';
 
 function App() {
-  return <div className="App" />;
+  const options = {
+    account: {
+      value: 'Account',
+    },
+    appearance: {
+      value: 'Appearance',
+    },
+    notifications: {
+      value: 'Notifications',
+    },
+    about: {
+      value: 'About',
+    },
+  };
+
+  return (
+    <div className="App">
+      <Tabs
+        options={options}
+        defaultActive="account"
+        onClick={active => console.log(active)}
+      />
+    </div>
+  );
 }
 
 export default App;
