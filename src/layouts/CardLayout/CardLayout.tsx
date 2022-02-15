fd48c362aa30f39f44ec9c4bacec2a372d08f474
karinacrmcldev <perfechate@gmail.com>
2022-02-15 16:56:05 +0200

    Created a layout for cards

--- src/layouts/CardLayout/CardLayout.tsx
+++ src/layouts/CardLayout/CardLayout.tsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
+import s from './CardLayout.module.scss';
 
-export function CardLayout() {
-  return <div />;
+type Props = {
+  children: ReactNode;
+  title: string;
+  description?: string;
+};
+
+export function CardLayout({ children, title, description }: Props) {
+  return (
+    <div className={s.card_container}>
+      <h4 className={s.card_title}>{title}</h4>
+      {description && <p className={s.card_descirpiton}>{description}</p>}
+
+      <div className={s.card_content}>{children}</div>
+    </div>
+  );
 }
