507651627650ac72d6efc04eb4faf583fde956f6
karinacrmcldev <perfechate@gmail.com>
2022-02-18 10:41:55 +0200

    PR-24: app.tsx cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
 import React from 'react';
-import { DashboardWordPacks } from './components/Dashboard/WordPacks/WordPacks';
-import { Sidebar } from './components/Sidebar/Sidebar/Sidebar';
-import { Button } from './components/UI/Button/Button';
-import { Logo } from './components/UI/Logo/Logo';
-import { Switch } from './components/UI/Switch/Switch';
-import { packs } from './mocks/packs';
 
 function App() {
-  return (
-    <div className="App">
-      <DashboardWordPacks packs={packs} />
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
