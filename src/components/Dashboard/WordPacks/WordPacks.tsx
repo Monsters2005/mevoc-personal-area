7eb3b76c41cd50886d0f3b68e24fa552f5b1a82d
karinacrmcldev <perfechate@gmail.com>
2022-02-18 09:26:54 +0200

    Created component with word packs

--- src/components/Dashboard/WordPacks/WordPacks.tsx
+++ src/components/Dashboard/WordPacks/WordPacks.tsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
+import s from './WordPacks.module.scss';
+import { CardLayout } from '../../../layouts/CardLayout/CardLayout';
+import { Pack } from './types';
+import { WordPack } from '../WordPack/WordPack';
 
-export function DashboardWordPacks() {
-  return <div />;
+type Props = {
+  packs: Pack[];
+};
+
+export function DashboardWordPacks({ packs }: Props) {
+  return (
+    <CardLayout title="Word packs">
+      <div className={s.wordpacks_container}>
+        {packs.map(item => (
+          <WordPack item={item} />
+        ))}
+      </div>
+    </CardLayout>
+  );
 }
---
+++ src/components/Dashboard/WordPacks/types.ts
@@ -0,0 +1,8 @@
+import { ReactNode } from 'react';
+
+export type Pack = {
+  name: string;
+  key: string;
+  words: Array<string>;
+  icon: string;
+};
