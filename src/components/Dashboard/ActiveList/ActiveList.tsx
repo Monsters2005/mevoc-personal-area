08b665eff9aa15ee36800640c3babed2e20f1562
karinacrmcldev <perfechate@gmail.com>
2022-03-01 16:51:37 +0200

    Added dnd

--- src/components/Dashboard/ActiveList/ActiveList.tsx
+++ src/components/Dashboard/ActiveList/ActiveList.tsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 import classNames from 'classnames';
+import { Draggable } from 'react-beautiful-dnd';
 import s from './ActiveList.module.scss';
 import { List } from '../ActiveLists/types';
 import { getWordsCount } from '../../../utils/getWordsCount';
 
 type Props = {
   item: List;
+  index: number;
 };
 
-export function ActiveList({ item }: Props) {
+export function ActiveList({ item, index }: Props) {
   const [selected, setSelected] = useState(false);
 
   function selectList() {
@@ -17,23 +19,31 @@ export function ActiveList({ item }: Props) {
   }
 
   return (
-    <div
-      className={classNames(s.activelist_container, {
-        [s.activelist_selected]: selected,
-      })}
-    >
-      <button
-        className={classNames(s.activelist_checkbox, {
-          [s.activelist_checkbox_selected]: selected,
-        })}
-        onClick={selectList}
-      >
-        <span />
-      </button>
-      <div className={s.activelist_content}>
-        <h4 className={s.activvelist_title}>{item.name}</h4>
-        <p>{getWordsCount(item.words.length)}</p>
-      </div>
-    </div>
+    <Draggable draggableId={item.key} index={index}>
+      {(provided, snapshot) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          className={classNames(s.activelist_container, {
+            [s.activelist_selected]: selected,
+            [s.activelist_dragging]: snapshot.isDragging,
+          })}
+        >
+          <button
+            className={classNames(s.activelist_checkbox, {
+              [s.activelist_checkbox_selected]: selected,
+            })}
+            onClick={selectList}
+          >
+            <span />
+          </button>
+          <div className={s.activelist_content}>
+            <h4 className={s.activvelist_title}>{item.name}</h4>
+            <p>{getWordsCount(item.words.length)}</p>
+          </div>
+        </div>
+      )}
+    </Draggable>
   );
 }
--- src/components/Dashboard/ActiveLists/ActiveLists.tsx
+++ src/components/Dashboard/ActiveLists/ActiveLists.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from 'react-beautiful-dnd';
 import { CardLayout } from '../../../layouts/CardLayout/CardLayout';
 import { lists } from '../../../mocks/lists';
 import { Button } from '../../UI/Button/Button';
 import { ActiveList } from '../ActiveList/ActiveList';
 import s from './ActiveLists.module.scss';
+import { reorder } from '../../../utils/reorder';
 
 export function DashboardActiveLists() {
+  const [items, setItems] = React.useState(lists);
+
+  const onDragEnd = ({ destination, source }: DropResult) => {
+    if (!destination) return;
+    const newItems = reorder(items, source.index, destination.index);
+    setItems(newItems);
+  };
+
   return (
     <CardLayout title="Active Lists">
       <Button
@@ -20,17 +35,26 @@ export function DashboardActiveLists() {
           height: '30px',
           position: 'absolute',
           right: '25px',
-          top: '20px',
+          top: '18px',
         }}
       >
         Add
       </Button>
+
       <div className={s.activelists_container}>
-        {lists.map(item => (
-          <ActiveList item={item} />
-        ))}
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="droppable-list">
+            {provided => (
+              <div ref={provided.innerRef} {...provided.droppableProps}>
+                {items.map((item, index) => (
+                  <ActiveList item={item} index={index} key={item.key} />
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       </div>
-      ;
     </CardLayout>
   );
 }
