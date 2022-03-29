19f01769d48ac02b69bbbeeea06eed50c5f429d8
karinacrmcldev <perfechate@gmail.com>
2022-03-29 19:38:15 +0300

    Created avatar card for settings account tab

---
+++ src/components/Settings/AvatarCard/AvatarCard.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { getMediaLink } from '../../../utils/getMediaLink';
+import { Button } from '../../UI/Button/Button';
+
+import s from './AvatarCard.module.scss';
+import { SettingsSvgSelector } from './SettingsSvgSelector';
+
+type Props = {
+  avatar: string;
+  onEdit: () => void;
+  onDelete: () => void;
+};
+
+export function AvatarCard({ avatar, onEdit, onDelete }: Props) {
+  const avatarUrl = getMediaLink(avatar);
+
+  return (
+    <div className={s.avatarcard_container}>
+      <div className={s.avatarcard_image}>
+        <img src={avatarUrl} alt={avatar} />
+      </div>
+
+      <div className={s.avatarcard_buttons}>
+        <Button type="secondary" onClick={onDelete}>
+          <SettingsSvgSelector id="delete" />
+          DeleteAvatar
+        </Button>
+        <Button type="secondary" onClick={onEdit}>
+          <SettingsSvgSelector id="edit" />
+          Edit avatar
+        </Button>
+      </div>
+    </div>
+  );
+}
