5664746fca05485713faf01f9ce5b26d59641ef5
karinacrmcldev <perfechate@gmail.com>
2022-04-13 17:32:44 +0300

    PR-27: Fixes

--- src/components/User/Card/Card.tsx
+++ src/components/User/Card/Card.tsx
@@ -8,34 +8,46 @@ type Props = {
   userData: {
     firstName: string;
     lastName: string;
-    avatar: string;
+    avatar: string | null;
     location: string;
     langNative: string;
     langLearning: string;
   };
 };
 
-export function UserCard({ userData }: Props) {
+export function UserCard({
+  userData: {
+    firstName,
+    lastName,
+    avatar,
+    location,
+    langLearning,
+    langNative,
+  },
+}: Props) {
+  const avatarUrl = getMediaLink(avatar);
+
   return (
     <div className={s.usercard_container}>
       <div className={s.usercard_avatar}>
-        <img src={getMediaLink(userData.avatar)} alt={userData.avatar} />
+        <img
+          src={
+            avatarUrl !== ' '
+              ? avatarUrl
+              : "https://i.ibb.co/FXPtHCX/profile-1.png'"
+          }
+          alt="avatar"
+        />
       </div>
       <div className={s.usercard_info}>
-        <h4>{`${userData.firstName} ${userData.lastName}`}</h4>
+        <h4>{`${firstName} ${lastName}`}</h4>
         <p>
           <GlobalSvgSelector id="location" />
-          {userData.location}
+          {location}
         </p>
 
-        <LanguageField
-          label="Native Language"
-          language={userData.langNative}
-        />
-        <LanguageField
-          label="Currently Learning"
-          language={userData.langLearning}
-        />
+        <LanguageField label="Native Language" language={langNative} />
+        <LanguageField label="Currently Learning" language={langLearning} />
       </div>
     </div>
   );
