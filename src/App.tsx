feaf9ce7c20aec7d2ab1a026a43148a36ceb1a3a
karinacrmcldev <perfechate@gmail.com>
2022-04-13 21:48:51 +0300

    PR-12:Cleanup in app, added storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,7 @@
 import React from 'react';
-import { Button } from './components/UI/Button/Button';
-import { Tabs } from './components/UI/Tabs/Tabs';
 
 function App() {
-  const options = {
-    account: {
-      value: 'Account',
-    },
-    appearance: {
-      value: 'Appearance',
-    },
-    notifications: {
-      value: 'Notifications',
-    },
-    about: {
-      value: 'About',
-    },
-  };
-
-  return (
-    <div className="App">
-      <Tabs
-        options={options}
-        defaultActive="account"
-        onClick={active => console.log(active)}
-      />
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
