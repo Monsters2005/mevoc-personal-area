306a39467555bec62b44293a9b9a2a043608fb35
karinacrmcldev <perfechate@gmail.com>
2022-04-15 12:56:05 +0300

    PR-30: Fixed function and src

--- src/components/Settings/AvatarCard/AvatarCard.tsx
+++ src/components/Settings/AvatarCard/AvatarCard.tsx
@@ -4,6 +4,7 @@ import { Button } from '../../UI/Button/Button';
 
 import s from './AvatarCard.module.scss';
 import { SettingsSvgSelector } from './SettingsSvgSelector';
+import defaultAvatar from '../../../assets/images/defaultAvatar.png';
 
 type Props = {
   avatar: string;
@@ -12,25 +13,18 @@ type Props = {
 };
 
 export function AvatarCard({ avatar, onEdit, onDelete }: Props) {
-  const avatarUrl = getMediaLink(avatar);
+  const avatarUrl = getMediaLink(avatar) || defaultAvatar;
 
   return (
     <div className={s.avatarcard_container}>
       <div className={s.avatarcard_image}>
-        <img
-          src={
-            avatarUrl !== ' '
-              ? avatarUrl
-              : "https://i.ibb.co/FXPtHCX/profile-1.png'"
-          }
-          alt={avatar}
-        />
+        <img src={avatarUrl} alt={avatar} />
       </div>
 
       <div className={s.avatarcard_buttons}>
         <Button type="secondary" onClick={onDelete}>
           <SettingsSvgSelector id="delete" />
-          DeleteAvatar
+          Delete Avatar
         </Button>
         <Button type="secondary" onClick={onEdit}>
           <SettingsSvgSelector id="edit" />
--- src/utils/getMediaLink.ts
+++ src/utils/getMediaLink.ts
@@ -1,3 +1,5 @@
 export function getMediaLink(fileName: string) {
-  return `https://i.ibb.co/${fileName}`; // TODO: add url from axios
+  const isValid = fileName !== '' && !/^\s*$/.test(fileName);
+
+  return isValid ? `https://i.ibb.co/${fileName}` : undefined; // TODO: add url from axios
 }
