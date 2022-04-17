64013e18d4d6ae7399d9ba9ca7e5a311f4bc6d26
karinacrmcldev <perfechate@gmail.com>
2022-04-17 23:39:21 +0300

    PR-6: Added error

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -4,8 +4,8 @@ import React, {
   FocusEventHandler,
   FormEventHandler,
   ReactNode,
-  useState,
 } from 'react';
+import { GlobalSvgSelector } from '../../../shared/GlobalSvgSelector';
 import s from './Input.module.scss';
 
 type Props = {
@@ -14,11 +14,13 @@ type Props = {
   children?: ReactNode;
   icon?: ReactNode;
   styles?: CSSProperties;
-  onChange: FormEventHandler<HTMLInputElement>;
-  onBlur: FocusEventHandler<HTMLInputElement>;
+  onChange?: FormEventHandler<HTMLInputElement>;
+  onBlur?: FocusEventHandler<HTMLInputElement>;
   onFocus?: FocusEventHandler<HTMLInputElement>;
   onPaste?: ClipboardEventHandler<HTMLInputElement>;
   onInput?: FormEventHandler<HTMLInputElement>;
+  isError?: boolean;
+  error?: string;
 };
 
 export function Input({
@@ -32,21 +34,31 @@ export function Input({
   onFocus,
   onInput,
   onPaste,
+  isError,
+  error,
 }: Props) {
   return (
     <div className={s.input_container}>
-      <span className={s.input_icon}>{icon}</span>
-      <input
-        style={{ ...styles }}
-        value={value}
-        placeholder={placeholder}
-        onChange={onChange}
-        onBlur={onBlur}
-        onFocus={onFocus}
-        onInput={onInput}
-        onPaste={onPaste}
-      />
-      <span className={s.input_content}>{children}</span>
+      <div className={s.input_top}>
+        <span className={s.input_icon}>{icon}</span>
+        <input
+          style={{ ...styles, paddingLeft: icon ? '50px' : '0' }}
+          value={value}
+          placeholder={placeholder}
+          onChange={onChange}
+          onBlur={onBlur}
+          onFocus={onFocus}
+          onInput={onInput}
+          onPaste={onPaste}
+        />
+        <span className={s.input_content}>{children}</span>
+      </div>
+      {isError && (
+        <div className={s.input_error}>
+          <GlobalSvgSelector id="error" />
+          <p>{error}</p>
+        </div>
+      )}
     </div>
   );
 }
