56e774c731c6d89ff12ad5856116483824a4cbdf
Vanya Samarin <56263427+mirageN1349@users.noreply.github.com>
2022-04-18 10:39:52 +0300

    Update getMediaLink.ts

--- src/utils/getMediaLink.ts
+++ src/utils/getMediaLink.ts
@@ -1,5 +1,5 @@
-export function getMediaLink(fileName: string) {
+export function getMediaLink(fileName: string): string {
   const isValid = fileName !== '' && !/^\s*$/.test(fileName);
 
-  return isValid ? `https://i.ibb.co/${fileName}` : undefined; // TODO: add url from axios
+  return isValid ? `https://i.ibb.co/${fileName}` : ""; // TODO: add url from axios
 }
