f97266b0581955fb84b649542684ce623f76a147
karinacrmcldev <perfechate@gmail.com>
2022-04-28 00:37:23 +0300

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
 import React from 'react';
-import { DashboardActiveLists } from './components/Dashboard/ActiveLists/ActiveLists';
-import { list } from './mocks/list';
 
 function App() {
-  return (
-    <div className="App">
-      <div style={{ width: '100%' }}>
-        <DashboardActiveLists
-          lists={[list]}
-          onAddList={() => console.log('add')}
-        />
-      </div>
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
