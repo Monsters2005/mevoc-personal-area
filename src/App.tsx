1d43e44309f804a6a324daabf8781111e993edf0
karinacrmcldev <perfechate@gmail.com>
2022-04-28 00:31:24 +0300

    Fixed types

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { DashboardActiveLists } from './components/Dashboard/ActiveLists/ActiveLists';
+import { list } from './mocks/list';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <div style={{ width: '100%' }}>
+        <DashboardActiveLists
+          lists={[list]}
+          onAddList={() => console.log('add')}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
