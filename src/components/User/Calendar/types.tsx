4f29267b478e68892e34b3e747fb10b563b86131
karinacrmcldev <perfechate@gmail.com>
2022-05-10 22:28:26 +0300

    Fixes

--- src/components/User/Calendar/types.tsx
+++ src/components/User/Calendar/types.tsx
@@ -2,3 +2,8 @@ export type Dates = {
   startDate: Date;
   endDate: Date;
 };
+
+export type Tab = {
+  key: string;
+  value: string;
+};
---
+++ src/constants/calendarTabs.ts
@@ -0,0 +1,16 @@
+import { Tab } from '../components/User/Calendar/types';
+
+export const calendarTabs: Tab[] = [
+  {
+    key: 'week',
+    value: 'Week',
+  },
+  {
+    key: 'month',
+    value: 'Month',
+  },
+  {
+    key: 'year',
+    value: 'Year',
+  },
+];
