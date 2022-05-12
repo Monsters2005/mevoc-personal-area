d92c95f7b3bff14efa4a1629987a363011fef3c8
karinacrmcldev <perfechate@gmail.com>
2022-05-12 20:36:18 +0300

    Created forgot password form

---
+++ src/@types/dto/auth/forgot-password.dto.ts
@@ -0,0 +1,3 @@
+export type ForgotPasswordDto = Readonly<{
+  email: string;
+}>;
---
+++ src/@types/dto/auth/signup.dto.ts
@@ -0,0 +1,5 @@
+export type SignUpDto = Readonly<{
+  firstName: string;
+  email: string;
+  password: string;
+}>;
